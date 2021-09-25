a2402d9b4e7b7bd9d682a694d2112a625b2b0c33
Warel123 <ppullabh22@students.d125.org>
2021-09-25 13:03:38 -0500

    first commit

---
+++ src/README.md
@@ -0,0 +1,2 @@
+# cararra-ctf
+
---
+++ src/theme.js
@@ -0,0 +1,23 @@
+import { red } from "@material-ui/core/colors";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#00ccff",
+    },
+    secondary: {
+      main: "#00ffcc",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#fff",
+    },
+  },
+});
+
+export default theme;
